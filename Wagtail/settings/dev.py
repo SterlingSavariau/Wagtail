71bcc1e93f5eda5a4a512d70e5de8abbd7cec01c
Sterling Savariau <sterlingsavariau@Sterlings-MacBook-Pro.local>
2021-11-12 11:41:16 -0500

    First comit. Created Wagtail, Installed Requirements, Migrated DB

---
+++ Wagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ha-nax$rm4#=@%*w+3qo9wl^@f3i8-&j4hkcy8&8l(656_^8ex'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

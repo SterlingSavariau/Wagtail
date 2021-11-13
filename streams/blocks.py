ba809f8a9e0fa2103f126605ec2b143d27d818c6
Sterling Savariau <sterlingsavariau@Sterlings-MacBook-Pro.local>
2021-11-13 10:30:04 -0500

    How to Add a RichText StreamField to your Wagtail CMS Page

--- streams/blocks.py
+++ streams/blocks.py
@@ -3,12 +3,37 @@
 from wagtail.core import blocks
 
 class TitleAndTextBlock(blocks.StructBlock):
-    #Title and text and nothing else.
+    # Title and text and nothing else.
 
-    title = blocks.CharBlock(required=True, help_text='Add your title')
-    text = blocks.TextBlock(required=True, help_text='Add additional text')
+    title = blocks.CharBlock(required=True, help_text="Add your title")
+    text = blocks.TextBlock(required=True, help_text="Add additional text")
 
-    class Meta: # noqa
+    class Meta:  # noqa
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title & Text"
+
+
+class RichTextBlock(blocks.RichTextBlock):
+    # RichText with all the features.
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full RichText"
+
+class SimpleRichTextBlock(blocks.RichTextBlock):
+    # RichText without (limited) all the features.
+
+    def __init__(self, required=True, help_text=None, editor="default", features=None, **kwargs):
+        super().__init__(**kwargs)
+        self.features = [
+            "bold", 
+            "italic", 
+            "link"
+        ]
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "edit"
+        label = "Simple RichText"
